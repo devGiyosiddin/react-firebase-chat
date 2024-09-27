20b8f4f51dd6f957121e2614370be8021abf40a2
devGiyosiddin <devgiyosiddin@gmail.com>
2024-09-27 15:00:06 +0500

    Fix mime-type problem v2.0

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pages/dist/",
+  base: "/react-firebase-chat/",
 })
 
