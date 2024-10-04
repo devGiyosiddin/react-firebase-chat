97c52634b96433e76ecdff274ddc961d6ebca6dd
devGiyosiddin <devgiyosiddin@gmail.com>
2024-10-04 21:27:42 +0500

    update JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import List from "./components/list/list";
-import Chat from "./components/chat/chat";
-import Detail from "./components/detail/detail";
-import Login from "./components/login/login";
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
