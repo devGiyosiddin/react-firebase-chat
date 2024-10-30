bf12adf729bedafa70c2b81216e1407bd0286866
devGiyosiddin <devgiyosiddin@gmail.com>
2024-10-30 17:14:54 +0500

    Delete video call icon

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -12,7 +12,6 @@ const UserInfo = () => {
             </div>
             <div className="icons">
                 <img src="./more.png" alt="" />
-                <img src="./video.png" alt="" />
                 <img src="./edit.png" alt="" />
             </div>
         </div>
